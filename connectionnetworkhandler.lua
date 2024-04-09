e9d3c0669e0ada7871e5ec810412ecbba8ca7a1b
vojin154 <vojin154@gmail.com>
2024-04-09 19:14:02 +0200

    Init

---
+++ connectionnetworkhandler.lua
@@ -0,0 +1,36 @@
+local old_func = ConnectionNetworkHandler.sync_outfit
+
+local skip_first = {}
+function ConnectionNetworkHandler:sync_outfit(outfit_string, outfit_version, outfit_signature, sender)
+    local peer = self._verify_sender(sender)
+
+	if not peer then
+		return
+	end
+
+    --Check if the peer has been loaded
+    if not skip_first[peer:id()] then
+        --If we don't do this then the character won't be loaded and there will be an empty space with a name tag
+        --After loading into the lobby when the peer is already in inventory
+        --So we skip the first check and let the original run for that matter for each connected peer
+        --No need to check for future connecting peers after we are loaded as those will be put into "lobby" state and will run the original
+        return old_func(self, outfit_string, outfit_version, outfit_signature, sender)
+    end
+
+    local state = managers.menu:get_peer_state(peer:id())
+
+    --If peer isn't in the ready state (lobby)
+    if state ~= "lobby" then
+        _G._stop_lobby_lag = _G._stop_lobby_lag or {}
+        --When syncing over and over this info will be overwritten, so no need to make other checks
+        _G._stop_lobby_lag[peer:id()] = {
+            outfit_string = outfit_string,
+            outfit_version = outfit_version,
+            outfit_signature = outfit_signature,
+            sender = sender
+        }
+        return
+    end
+
+    return old_func(self, outfit_string, outfit_version, outfit_signature, sender)
+end
---
+++ menumanager.lua
@@ -0,0 +1,18 @@
+Hooks:PostHook(MenuManager, "_set_peer_sync_state", "_set_peer_sync_state_stop_lobby_lag", function(self, peer_id, state)
+    if (state ~= "lobby") or (not peer_id) then
+        return
+    end
+
+    local sync_outfit_data = _G._stop_lobby_lag and _G._stop_lobby_lag[peer_id]
+
+    if sync_outfit_data then
+            local outfit_string = sync_outfit_data.outfit_string
+            local outfit_version = sync_outfit_data.outfit_version
+            local outfit_signature = sync_outfit_data.outfit_signature
+            local sender = sync_outfit_data.sender
+
+        --Call the sync function with the saved info as the function runs on item change
+        --Means we have to call it ourselves to load the character when they change state
+        managers.network._handlers.connection:sync_outfit(outfit_string, outfit_version, outfit_signature, sender)
+    end
+end)

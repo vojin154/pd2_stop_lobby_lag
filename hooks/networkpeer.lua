22ecd15f99e1ad16a8dfbd92b1aa901d03a6b58a
vojin154 <vojin154@gmail.com>
2024-04-12 00:32:53 +0200

    Now takes care of additional functions done by Beardlib

---
+++ hooks/networkpeer.lua
@@ -0,0 +1,32 @@
+local function hook_function(func_name)
+    --If the function exists, save it
+    local func = NetworkPeer[func_name]
+
+    --Since right now, we are trying to wrap functions, that are not originally in NetworkPeer, but added by Beardlib
+    --So we check if the function exists 
+    if func then
+        --If it exists override the function, to add a check if the player is ready
+        --If he is, then run the original function
+        NetworkPeer[func_name] = function(self, ...)
+            local state = managers.menu:get_peer_state(self:id())
+
+            if state and state == "lobby" then
+                return
+            end
+
+            return func(self, ...)
+        end
+    end
+end
+
+--Names of the functions we want to wrap
+local hooks = {
+    "set_outfit_string_beardlib",
+    "set_extra_outfit_string_beardlib",
+    "beardlib_reload_outfit",
+    "beardlib_reload_extra_outfit"
+}
+
+for i = 1, #hooks do
+    hook_function(hooks[i])
+end
